5a9ae317c105df43e04b937a64c90047282b4851
jcorreaCondorLabs <jcorrea@condorlabs.io>
2020-09-10 14:34:03 -0500

    Attempt 1

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * @function
+ * @param {String} friend - your friend's name
+ * @description This function just greets and nothing else
+ */
+const sayHello = friend => {
+    console.log('Hello' + friend)
+}
+
+sayHello('Dummy')
